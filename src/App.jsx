6de0a04ec61920ae992cb9a0af3e9c56e6ac624b
pradum6391-wq <pradum6391@gmail.com>
2026-06-26 22:38:00 +0530

    add cards

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+import Card from './components/card'
+ 
+ const App = () => {
+   return (
+     <div className='parentdv'>
+     <Card/>
+     <Card/>
+     <Card />
+     </div>
+   )
+ }
+ 
+ export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='dv'>
+        <div className='card'>
+            <img id='img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvvxTSEnrwX9bf4J96xrYmFsvUVPxbx434NP61hTlsrC8A6dsF9bQ1Wkpx&s=10" alt="" />
+            <h1>Pradum Maurya</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button className='btn'>Click Here</button>
+        </div>
+    </div>
+  )
+}
+
+export default Card
